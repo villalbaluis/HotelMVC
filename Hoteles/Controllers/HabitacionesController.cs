159e82525f74b9564c7bcb9f6664a3c0ba225d62
LUIS-VILLALBA\User <luisvillalba244@gmail.com>
2024-05-27 16:23:08 -0500

    Add: Habitaciones views TODO: Style for every view

--- Hoteles/Controllers/HabitacionesController.cs
+++ Hoteles/Controllers/HabitacionesController.cs
@@ -48,6 +48,97 @@ namespace Hoteles.Controllers
             ViewBag.IdHotel = new SelectList(_context.Hoteles, "IdHotel", "Nombre", habitacion.IdHotel);
             return View(habitacion);
         }
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            Habitacion habitacion = _context.Habitaciones.Find(id);
+            if (habitacion == null)
+            {
+                ViewBag.ErrorMessage = "Ocurrió un error consultando la habitación.";
+                ViewBag.IdHotel = new SelectList(_context.Hoteles, "IdHotel", "Nombre", habitacion.IdHotel);
+                return View();
+            }
+            ViewBag.IdHotel = new SelectList(_context.Hoteles, "IdHotel", "Nombre", habitacion.IdHotel);
+            return View(habitacion);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Habitacion habitacion)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Entry(habitacion).State = EntityState.Modified;
+                    _context.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                catch (Exception ex)
+                {
+                    if (ex.InnerException != null)
+                    {
+                        ViewBag.ErrorMessage = "Falló en edición de torneo, error: (" + ex.InnerException.Message + ").";
+                        ViewBag.IdHotel = new SelectList(_context.Hoteles, "IdHotel", "Nombre", habitacion.IdHotel);
+                        return View(habitacion);
+                    }
+                }
+            }
+            ViewBag.IdHotel = new SelectList(_context.Hoteles, "IdHotel", "Nombre", habitacion.IdHotel);
+            return View(habitacion);
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            Habitacion habitacion = _context.Habitaciones.Find(id);
+            if (habitacion == null)
+            {
+                ViewBag.ErrorMessage = "Error, la habitación no ha podido ser encontrado.";
+                return View();
+            }
+            return View(habitacion);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int? id)
+        {
+            Habitacion habitacion = _context.Habitaciones.Find(id);
+            if (habitacion != null)
+            {
+                try
+                {
+                    _context.Habitaciones.Remove(habitacion);
+                    _context.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.ErrorMessage = "Error, imposible eliminar la habitación, código de error: (" + ex.InnerException.Message + ").";
+                }
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return View();
+            }
+            Habitacion habitacion = _context.Habitaciones.Find(id);
+            return View(habitacion);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _context.Dispose();
+            }
+            base.Dispose(disposing);
+        }
     }
 
 }
